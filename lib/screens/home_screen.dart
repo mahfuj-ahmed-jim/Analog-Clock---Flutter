ab92c7df6e2c44bc50d530cade5c808809632e99
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2022-02-02 19:34:16 +0600

    AppBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:analog_clock/size_config.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    // we have to call this on our starting page
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset("assets/icons/Settings.svg"),
+          color: Theme.of(context).iconTheme.color,
+        ),
+        actions: const [
+          BuildAddButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class BuildAddButton extends StatelessWidget {
+  const BuildAddButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(10)),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          width: getProportionateScreenWidth(32),
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            shape: BoxShape.circle,
+          ),
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
